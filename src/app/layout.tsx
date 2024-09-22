ab7e5b437891508ca37d8909c2f2b35c15d1c986
Nikita Babko <niki.babko@gmail.com>
2024-09-22 18:16:07 +0700

    feat: initial commit
    1) Add eslint and prettier;
    2) Add commitlint and commitizen;
    3) Add base font;
    4) Add lintstaged;
    5) Add reset css;
    6) Add favicon.

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
+
+const openSansRegular = localFont({
+  src: './fonts/OpenSans-Regular.woff2',
+  variable: '--open-sans-regular',
+  weight: '400',
+});
+
+const openSansBold = localFont({
+  src: './fonts/OpenSans-Bold.woff2',
+  variable: '--open-sans-bold',
+  weight: '500',
+});
+
+export const metadata: Metadata = {
+  title: 'Personal blog',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${openSansRegular.variable} ${openSansBold.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
